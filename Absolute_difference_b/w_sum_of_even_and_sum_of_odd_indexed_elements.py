87dfb47f7bb003326f97f9b0e9caa4716e3ad1d2
seramdasusatya <97424415+seramdasusatya@users.noreply.github.com>
2022-06-29 11:27:11 +0530

    n=int(input()) arr=list(map(int,input().split())) s1=0 s2=0 for i in range(n):     if i%2==0:         s1=s1+arr[i]     elif i%2==1:         s2=s2+arr[i] print(abs(s1-s2))

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.py
@@ -0,0 +1,10 @@
+n=int(input())
+arr=list(map(int,input().split()))
+s1=0
+s2=0
+for i in range(n):
+    if i%2==0:
+        s1=s1+arr[i]
+    elif i%2==1:
+        s2=s2+arr[i]
+print(abs(s1-s2))     
